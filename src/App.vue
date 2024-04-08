d0946c33b3e951e6f812817b626938f9d5e685da
windowsair <msdn_02@sina.com>
2024-04-08 20:48:35 +0800

    feat(vue): Add content component
    The Content component is used for the main setup and status display sections.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <script setup>
-import navbar from './components/navbar.vue';
+import navbar from './components/navbar.vue'
+import content from './components/content.vue'
 
 </script>
 
 <template>
   <header>
-    <navbar />
+    <navbar class="navbar"/>
   </header>
+  <content class="content" />
 </template>
 
 <script>
 </script>
 
-<style scoped></style>
+<style>
+.navbar {
+  height: 55px;
+}
+
+.content {
+  height: calc(100vh - 55px);
+}
+
+.content .el-tabs__header {
+  min-height: 40px;
+}
+
+.content .el-tabs__content {
+  height: calc(100vh - 95px);
+}
+
+.content .el-tab-pane {
+  height: 100%;
+}
+
+.content .el-tabs--border-card {
+  border: none;
+}
+</style>
---
+++ src/components/content.vue
@@ -0,0 +1,157 @@
+<template>
+    <div>
+        <el-tabs type="border-card">
+            <el-tab-pane label="DAP Setting" class="h-full">
+                <div class="mb-2 flex items-center text-sm">
+                    <el-radio-group v-model="isRemoteDAP" class="ml-4">
+                        <el-radio :value="true" size="large">Remote</el-radio>
+                        <el-radio :value="false" size="large">USB</el-radio>
+                    </el-radio-group>
+                </div>
+                <div class="mb-2 flex ml-4">
+                    <el-input v-if="isRemoteDAP" v-model="dapURI" style="width: 240px" placeholder="dap.local" />
+                </div>
+                <div class="mb-2 flex ml-4 mt-4">
+                    <el-button type="primary" :disabled="isDAPConnected">
+                        {{ isDAPConnected ? "Connected" : isRemoteDAP ? "Connect" : "Select Device" }}
+                    </el-button>
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="Flash Setting">
+                <div class="m-4">
+                    <p class="md-4 ela-text">Select Target Device</p>
+                    <el-cascader class="mt-2" placeholder="Type to search:" :options="deviceIndexOption" filterable
+                        v-model="targetDevice" />
+                </div>
+                <div class="m-4">
+                    <p class="md-4 ela-text">Max Clock</p>
+                    <el-cascader class="mt-2" :options="clockOptions" v-model="downloadOption.clock" />
+                </div>
+                <div class="mb-2 flex items-center text-sm">
+                    <el-radio-group v-model="downloadOption.erase" class="ml-4">
+                        <el-radio value="full" size="large">Erase Full Chip</el-radio>
+                        <el-radio value="part" size="large">Erase Sectors</el-radio>
+                        <el-radio value="none" size="large">Do Not Erase</el-radio>
+                    </el-radio-group>
+                </div>
+
+                <div class="mb-2 flex items-center text-sm ml-4">
+                    <el-checkbox v-model="downloadOption.program" label="Program" size="large" />
+                    <el-checkbox v-model="downloadOption.verify" label="Verify" size="large" />
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="Firmware Upload">
+                <el-container direction="vertical" class="h-full">
+                    <div class="flex-none">
+                        <el-upload drag :limit="1" :file-list="uploadFileList" :on-change="loadJsonFromFile"
+                            :auto-upload="false" multiple :show-file-list="false">
+
+                            <div class="upload-input">
+                                <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+                                <div class="el-upload__text">
+                                    Drop file here or <em>click to upload</em>
+                                </div>
+
+                            </div>
+                        </el-upload>
+                    </div>
+                    <div class="mt-2 flex-auto h-full">
+                        <el-input v-model="input" class="msg-output w-full h-full" type="textarea" resize="none"
+                            :readonly="true" />
+                    </div>
+                    <div class="upload-start h-1/10">
+                        <el-button class="mt-4" type="success" @click="startFlash">
+                            Start to Flash
+                        </el-button>
+                    </div>
+                </el-container>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<style>
+.msg-output>textarea {
+    height: 100%;
+}
+</style>
+
+<script setup>
+import { UploadFilled } from '@element-plus/icons-vue'
+import { ref, reactive } from 'vue'
+import { useStorage } from '@vueuse/core'
+import deviceIndexOption from '../device/deviceIndex.json'
+
+const isRemoteDAP = useStorage('remote-dap', false)
+const dapURI = useStorage('dap-uri', '')
+const isDAPConnected = ref(false)
+const targetDevice = useStorage('target-device', [])
+const downloadOption = useStorage('download-option', {
+    clock: 10000000,
+    erase: 'none',
+    program: true,
+    verify: true,
+})
+
+const uploadFileList = ref()
+let firmware;
+const isELFFormat = ref(false)
+
+const loadJsonFromFile = (file, fileList) => {
+    uploadFileList.value = fileList
+}
+
+const startFlash = () => {
+    let reader = new FileReader()
+    reader.readAsArrayBuffer(uploadFileList.value[0].raw)
+    reader.onload = (e) => {
+        console.log('done')
+    }
+}
+
+const clockOptions = [
+    {
+        value: 10000000,
+        label: '10MHz'
+    },
+    {
+        value: 5000000,
+        label: '5MHz'
+    },
+    {
+        value: 1000000,
+        label: '1MHz'
+    },
+    {
+        value: 500000,
+        label: '500kHz'
+    },
+    {
+        value: 200000,
+        label: '200kHz'
+    },
+    {
+        value: 100000,
+        label: '100kHz'
+    },
+    {
+        value: 50000,
+        label: '50kHz'
+    },
+    {
+        value: 20000,
+        label: '20kHz'
+    },
+    {
+        value: 10000,
+        label: '10kHz'
+    },
+    {
+        value: 5000,
+        label: '5kHz'
+    },
+]
+
+const input = ref('Wait to flash...\n')
+
+</script>
