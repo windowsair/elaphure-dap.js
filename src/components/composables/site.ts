849658d13303b4997e67961c57297d1eda68b3f4
windowsair <msdn_02@sina.com>
2024-05-24 23:04:38 +0800

    fix(vue): Redirect to 8080 port when using https and remote device
    Once we are in the https context, we can't just switch to http on
    port 80. So we try to use port 8080, where the web server will use
    a 301 redirect to port 80.

--- src/components/composables/site.ts
+++ src/components/composables/site.ts
@@ -12,13 +12,23 @@ export function isHttps(location: Location = window.location) {
 }
 
 export function redirectToHttp() {
-  if (window.location.protocol == 'https:') {
-    window.location.href = window.location.href.replace(/^https:/, 'http:')
+  if (window.location.protocol === 'https:') {
+    const url: URL = new URL(window.location.href)
+
+    /**
+     * Once we are in the https context, we can't just switch to http on port 80.
+     * So we try to use port 8080, where the web server will use a 301 redirect
+     * to port 80.
+     */
+    url.port = '8080'
+    url.protocol = 'http:'
+
+    window.location.href = url.toString()
   }
 }
 
 export function redirectToHttps() {
-  if (window.location.protocol == 'http:') {
-    window.location.href = window.location.href.replace(/^http:/, 'https:')
+  if (window.location.protocol === 'http:') {
+    window.location.protocol = 'https:'
   }
 }
